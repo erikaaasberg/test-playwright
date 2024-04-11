5424f62b72ac3fb0b9f4f5d3339534bd6772793d
erikaaasberg <erika.aasberg@gmail.com>
2024-04-11 12:58:45 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default async function Home() {
+  const hello = await getData();
+  
+  return (
+   <h1>{hello.hei}</h1>
+  );
+}
+
+async function getData() {
+  const res = await fetch("http://localhost:31337/hei")
+  const data = await res.json()
+
+  return data
+}
